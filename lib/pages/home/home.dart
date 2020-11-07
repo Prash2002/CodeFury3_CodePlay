bdb3993f944f763ff58717c3daf717e0a9f0f8aa
Prashu <1707prash@gmail.com>
2020-11-08 02:23:37 +0530

    commit

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,28 +1,28 @@
-import 'package:CodeFury3/models/user.dart';
-import 'package:flutter/material.dart';
-import 'package:google_sign_in/google_sign_in.dart';
+// import 'package:CodeFury3/models/user.dart';
+// import 'package:flutter/material.dart';
+// import 'package:google_sign_in/google_sign_in.dart';
 
-final GoogleSignIn googleSignIn = GoogleSignIn();
+// final GoogleSignIn googleSignIn = GoogleSignIn();
 
-class Home extends StatefulWidget {
-  final User currentUser;
+// class Home extends StatefulWidget {
+//   final User currentUser;
 
-  Home({this.currentUser}) ;
+//   Home({this.currentUser}) ;
   
-  @override
-  _HomeState createState() => _HomeState();
-}
+//   @override
+//   _HomeState createState() => _HomeState();
+// }
 
-class _HomeState extends State<Home> {
+// class _HomeState extends State<Home> {
 
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      // child: RaisedButton(
-      //   child : Text('Logout'),
-      //   onPressed:(){ logout();}
-      // )
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       // child: RaisedButton(
+//       //   child : Text('Logout'),
+//       //   onPressed:(){ logout();}
+//       // )
+//     );
+//   }
+// }
